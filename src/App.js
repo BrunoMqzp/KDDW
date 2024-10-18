bf8dc3732845f751ce685b9031caeef08f015708
BrunoMqzp <A00834415@tec.mx>
2024-10-17 20:45:24 -0600

    CRITICAL UPDATE
    CHANGE TO ALL CODE

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import Login from "./components/Login";
+import Crud from "./components/Crud";
+import Home from "./components/Home";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Dashboard from "./components/Dashboard";
+import Juego from "./components/Juego";
 
-function App() {
+
+const App = () => {
+  const [auth, setAuth] = useState({
+    isAuthenticated: false,
+    role: null,
+    persNo: null
+  });
+
+  // Cargar el estado de autenticación desde localStorage al montar el componente
+  useEffect(() => {
+    const storedAuth = localStorage.getItem('auth');
+    if (storedAuth) {
+      setAuth(JSON.parse(storedAuth));
+    }
+  }, []);
+
+  // Guardar el estado de autenticación en localStorage cada vez que cambia
+  useEffect(() => {
+    localStorage.setItem('auth', JSON.stringify(auth));
+  }, [auth]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          path="/home"
+          element={
+            <ProtectedRoute auth={auth.isAuthenticated}>
+              <Home auth={auth} />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/juego"
+          element={
+            <ProtectedRoute auth={auth.isAuthenticated}>
+              <Juego />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute auth={auth.isAuthenticated && auth.role === 'admin'}>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/crud"
+          element={
+            <ProtectedRoute auth={auth.isAuthenticated && auth.role === 'admin'}>
+              <Crud />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/login"
+          element={<Login setAuth={setAuth} />}
+        />
+        <Route path="*" element={<Navigate to="/login" />} /> {/* Redirección por defecto */}
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
